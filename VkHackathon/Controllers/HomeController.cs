9739a41b69080d3a588849d2e37aa9f36217e286
Trixic <hi.trixic@gmail.com>
2017-10-20 18:39:29 +0300

    add mobile view

--- VkHackathon/Controllers/HomeController.cs
+++ VkHackathon/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace VkHackathon.Controllers
             return View();
         }
 
+        public ActionResult IndexMobile()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
