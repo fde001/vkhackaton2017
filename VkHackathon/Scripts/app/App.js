d194d7de163a088f6a484f8a1b8f224bcce18e76
Trixic <hi.trixic@gmail.com>
2017-10-21 11:40:42 +0300

    add places details modal

--- VkHackathon/Scripts/app/App.js
+++ VkHackathon/Scripts/app/App.js
@@ -1,5 +1,6 @@
 ﻿var map;
-var lastQuery="";
+var lastQuery = "";
+var places;
 
 
 //VK.callMethod("setTitle", "Changed title");
@@ -25,7 +26,7 @@ function funcSearch() {
         function (data) {
 
             data = data || [];
-            var places = data.filter(x => x.photo != null &&
+            places = data.filter(x => x.photo != null &&
                 ((x.start && x.end && x.end > new Date()) ||
                     (x.start && !x.end && x.end > new Date()) ||
                     (!x.start && !x.end))
@@ -43,15 +44,33 @@ function funcSearch() {
 
                     $places.append(`<tr id="${places[i].id}"> <td><img src="${places[i].photo}"></td><td><a target="_blank" href="${places[i].url}">${places[i].title}</a></td><td>${places[i].distance} м</td></tr>`);
 
-                    map.geoObjects
-                        .add(new ymaps.Placemark([places[i].lat, places[i].lon],
-                            {
-                                balloonContent: `<img src="${places[i].photo}"><a target="_blank" href="${places[i].url}">${places[i].title}</a>`
-                            },
-                            {
-                                preset: 'islands#icon',
-                                iconColor: '#0095b6'
-                            }));
+                    var placemark = new ymaps.Placemark([places[i].lat, places[i].lon],
+                        {
+                            //balloonContent: `<img src="${places[i].photo}"><a target="_blank" href="${places[i].url}">${places[i].title}</a>`
+                        },
+                        {
+                            preset: 'islands#icon',
+                            iconColor: '#0095b6'
+                        });
+
+                    function addPlaceClick(i) {
+                        placemark.events.add('click', function () {
+
+                            $("#placeModal .modal-title").text(places[i].title);
+
+                            $("#placeLink").attr("href", places[i].url);
+                            $("#placeLink").attr("target", "_blank");
+                            $("#placeLink").text(places[i].title);
+
+                            $("#placeImg").attr("src", places[i].photo);
+
+                            $("#placeModal").modal("show");
+                        });
+                    }
+
+                    addPlaceClick(i);
+
+                    map.geoObjects.add(placemark);
                 }
             }
 
